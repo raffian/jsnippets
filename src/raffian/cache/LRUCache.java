6b7de4c2d1e9d5abeddfb9a2bad7cbe307fe32da
Raffi Basmajian <raffi@raffianphoto.com>
2015-02-07 20:32:18 -0500

    initial

---
+++ src/raffian/cache/LRUCache.java
@@ -0,0 +1,16 @@
+package raffian.cache;
+
+public class LRUCache {
+
+   private final int MAX_ENTRIES;
+   
+   public static void main(String[] args) {
+      
+
+   }
+   
+   public LRUCache(int maxEntries){
+      this.MAX_ENTRIES = maxEntries;
+   }
+
+}
