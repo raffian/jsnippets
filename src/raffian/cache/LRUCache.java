a200f0ff7580a9c21a5105bcf819808eaa50115e
Raffi Basmajian <raffi@raffianphoto.com>
2015-02-08 18:36:48 -0500

    completed implementation

--- src/raffian/cache/LRUCache.java
+++ src/raffian/cache/LRUCache.java
@@ -1,16 +1,70 @@
 package raffian.cache;
 
-public class LRUCache {
+import java.util.HashMap;
+import java.util.LinkedList;
 
-   private final int MAX_ENTRIES;
-   
-   public static void main(String[] args) {
-      
+/**
+ * Simple LRU cache implementation; uses LinkedList
+ * to track recently used keys, HashMap as backing cache.
+ * 
+ * This class is not synchronized; for thread-safe
+ * version, see ConcurrentLRUCache.
+ * 
+ * @author Raffi Basmajian
+ * @date   2/8/2014
+ * 
+ */
+public class LRUCache<K,V> {
 
+   private final LinkedList<K> recentKeys;
+   private final HashMap<K,V> cache;
+   private final int maxEntries;
+
+   public LRUCache(
+         int maxEntries
+   ){
+      this.maxEntries = maxEntries;
+      this.recentKeys = new LinkedList<>();
+      this.cache = new HashMap<>();
    }
    
-   public LRUCache(int maxEntries){
-      this.MAX_ENTRIES = maxEntries;
+   public void set(K key, V value){
+      V previous = cache.put(key, value); 
+      //remove existing key
+      if(previous != null)               
+         recentKeys.remove(key);      
+      //discard last if at max capacity
+      if(recentKeys.size() >= maxEntries)
+         recentKeys.removeLast();      
+      //update with most recent used key
+      recentKeys.add(0, key);
+   }
+  
+   public V get(K key){
+      V value = cache.get(key);
+      if(value != null){
+         recentKeys.remove(key);
+         recentKeys.add(0, key);         
+      }      
+      return value;               
+   }
+   
+   public String toString(){
+      return String
+               .format("recentKeys:%s, cache:%s",
+                  recentKeys.toString(),
+                  cache.toString());
+   }
+   
+   public static void main(String[] args) {
+      LRUCache<String,String > cache = new LRUCache<>(3);
+      cache.set("k1", "1");
+      cache.set("k2", "2");
+      cache.set("k1", "1");
+      cache.set("k3", "3");
+      cache.set("k4", "4");
+      
+      System.out.println(cache);
    }
 
 }
