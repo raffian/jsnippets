8f39ebe990866ddc72576c51c51ac7e6a49c61c2
Raffi Basmajian <raffi@raffianphoto.com>
2015-02-08 23:03:15 -0500

    added annotations

--- src/raffian/cache/LRUCache.java
+++ src/raffian/cache/LRUCache.java
@@ -2,20 +2,21 @@ package raffian.cache;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import javax.annotation.Nonnull;
 
 /**
- * Simple LRU cache implementation; uses LinkedList
- * to track recently used keys, HashMap as backing cache.
+ * LRU cache implementation showing nuts and bolts of LRU. 
+ * Uses LinkedList to track recently used keys, and HashMap 
+ * as backing cache. See LRULeanCache for simpler implementation.
  * 
  * This class is not synchronized; for thread-safe version, 
- * see ConcurrentLRUCache.
+ * see LRUConcurrentCache.
  * 
- * @author Raffi Basmajian
- * @date   2/8/2014
- * 
+ * @author  Raffi Basmajian
+ * @date    2/8/2014
+ * @version $Id$
  */
 public class LRUCache<K,V> {
-
    private final LinkedList<K> recentKeys;
    private final HashMap<K,V> cache;
    private final int maxEntries;
@@ -28,7 +29,7 @@ public class LRUCache<K,V> {
       this.cache = new HashMap<>();
    }
    
-   public void set(K key, V value){
+   public void put(@Nonnull K key, V value){
       V previous = cache.put(key, value); 
       //remove existing key
       if(previous != null)               
@@ -36,44 +37,44 @@ public class LRUCache<K,V> {
       //discard last if at max capacity
       if(recentKeys.size() >= maxEntries)
          recentKeys.removeLast();      
-      //update with most recent used key
-      recentKeys.add(0, key);
+      //set most recent key to front of list
+      setMostRecentKey(key);
    }
   
-   public V get(K key){
+   public V get(@Nonnull K key){
       V value = cache.get(key);
       if(value != null){
          recentKeys.remove(key);
-         recentKeys.add(0, key);         
+         setMostRecentKey(key);         
       }      
       return value;               
    }
    
-   public String toString(){
-      return String
-               .format("recentKeys-> %s, cache-> %s",
-                  recentKeys.toString(),
-                  cache.toString());
+   protected void setMostRecentKey(@Nonnull K key){
+      recentKeys.add(0, key);
    }
    
    public String recentKeysToString(){
       return recentKeys.toString();
    }
    
-   public String cacheToString(){
-      return cache.toString();
-   }
-   
    public static void main(String[] args) {
       LRUCache<String,String > cache = new LRUCache<>(3);
-      cache.set("k1", "1");
-      cache.set("k2", "2");
-      cache.set("k1", "1");
-      cache.set("k3", "3");
-      cache.set("k4", "4");
+      cache.put("k1", "1");
+      cache.put("k2", "2");
+      cache.put("k1", "1");
+      cache.put("k3", "3");
+      cache.put("k4", "4");
+          
+      assert cache.recentKeysToString().equals("[k4, k3, k1]");      
+      System.out.println(cache.recentKeysToString());
       
-      System.out.println(cache);      
-      assert cache.recentKeysToString().equals("[k4, k3, k1]");
-      assert cache.cacheToString().equals("{k1=1, k2=2, k3=3, k4=4}");
+      cache.get("k3");
+      assert cache.recentKeysToString().equals("[k3, k4, k1]");  
+      System.out.println(cache.recentKeysToString());
+      
+      cache.put("k1", "1");      
+      assert cache.recentKeysToString().equals("[k1, k3, k4]");  
+      System.out.println(cache.recentKeysToString());      
    }
 }
