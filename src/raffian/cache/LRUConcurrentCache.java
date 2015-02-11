87b86437f69de76148567aa70b28727c5d30ccb5
Raffi Basmajian <raffi@raffianphoto.com>
2015-02-10 23:11:54 -0500

    udpated with ReadWriteLock

--- src/raffian/cache/LRUConcurrentCache.java
+++ src/raffian/cache/LRUConcurrentCache.java
@@ -3,43 +3,60 @@ package raffian.cache;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * LRU cache implementation that's thread safe, uses 
  * ReentrantReadWriteLock instead of Collections.synchronizedMap()
- * for improved read throughput, assuming cache is read-mostly.
  *
  * @author  Raffi Basmajian
- * @date    2/8/2014
+ * @date    2/10/2014
  */
 @ThreadSafe
 public class LRUConcurrentCache<K,V> {   
    private final Map<K,V> cache;   
    private final int maxEntries;
-
+   private final ReadWriteLock readWriteLock;
+   private final Lock readLock;
+   private final Lock writeLock;
+   
    public LRUConcurrentCache(
          int maxEntries
    ){
-      this.maxEntries = maxEntries;      
-      //TODO change this to ReentrantReadWriteLock
-      this.cache = Collections.synchronizedMap(
-            new LinkedHashMap<K,V>(this.maxEntries, 0.75F, true){                       
+      this.maxEntries = maxEntries;            
+      this.cache = new LinkedHashMap<K,V>(this.maxEntries, 0.75F, true){                       
                private static final long serialVersionUID = -2926481390177591390L;
                @Override
                protected boolean removeEldestEntry(Map.Entry<K,V> eldest){            
                   return size() > maxEntries;
                }
-            });
+            };
+      this.readWriteLock = new ReentrantReadWriteLock(true);
+      this.readLock = readWriteLock.readLock();
+      this.writeLock = readWriteLock.writeLock();
    }
    
    public void put(K key, V value){
-      cache.put(key, value);
+      writeLock.lock();
+      try{
+         cache.put(key, value);
+      }finally{
+         writeLock.unlock();
+      }
    }
   
    public V get(@Nonnull K key){
-      return cache.get(key);               
+      readLock.lock();
+      try{
+         return cache.get(key);
+      }finally{
+         readLock.unlock();
+      }
    }
    
    public String toString(){
